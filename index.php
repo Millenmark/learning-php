f44887f8b350b46d0061ad01b9256459f67bd348
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-21 20:02:59 +0800

    imported: Database.php

--- index.php
+++ index.php
@@ -1,31 +1,11 @@
 <?php
 require "./utils/functions.php";
 require "router.php";
-
-// connect to our MySQL database. dsn = data source name
-class Database 
-{
-  public $connection;
-
-  public function __construct()
-  {
-    $dsn = "mysql:host=localhost;port=3306;dbname=simplecrud;charset=utf8mb4;user=root;password=myAdmin0372021";
-    $this->connection = new PDO($dsn);
-  }
-
-  public function query($query)
-  {
-
-    $statement = $this->connection->prepare($query);
-    $statement->execute();
-
-    return $statement->fetchAll(PDO::FETCH_ASSOC);
-  }
-}
+require "config/Database.php";
 
 $db = new Database();
 
-$posts = $db->query("SELECT * FROM posts");
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
 
 
