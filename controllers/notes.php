f36e02fa9afad7501b772f8b0d7db868be1497b3
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-28 18:11:56 +0800

    update

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,5 +1,10 @@
 <?php
 
-$heading = "Notes";
+$config = require("config/config.php");
+$db = new Database($config["database"]);
+
+$heading = "My Notes";
+
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 3")->fetchAll();
 
 require "./views/notes.view.php";
