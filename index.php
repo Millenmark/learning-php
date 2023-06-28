45729ad509fad319c0b48cb79ea63d0ea8835483
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-28 18:11:05 +0800

    update

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 require "./utils/functions.php";
-require "router.php";
 require "config/Database.php";
+require "router.php";
 
-$config = require("config/config.php");
 
-$db = new Database($config["database"]);
 
-$id = $_GET["id"];
-
-$query = "SELECT * FROM posts WHERE id = ?";
-
-$post = $db->query($query, [$id])->fetch();
-
-echo "<li>" . $post["title"] . "</li>";
 
