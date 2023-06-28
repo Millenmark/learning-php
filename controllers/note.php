580352bbb6ab412631edc009b983184b5cb3aa88
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-28 19:06:00 +0800

    update: added authorization

--- controllers/note.php
+++ controllers/note.php
@@ -3,8 +3,19 @@
 $config = require("config/config.php");
 $db = new Database($config["database"]);
 
+$currentUserId = 3;
+
+
 $heading = "Note";
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_GET['id']])->fetch();
+$note = $db->query("SELECT * FROM notes WHERE id = :id", 
+  [
+    "id" => $_GET['id']
+  ])->fetch();
+
+
+if(!$note || $note["user_id"] !== $currentUserId) {
+  abort(Response::FORBIDDEN);
+}
 
 require "./views/note.view.php";
