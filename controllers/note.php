7b051e6cddfb279e2cbc2b82bc71ce2ec7f84767
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-28 20:27:29 +0800

    update

--- controllers/note.php
+++ controllers/note.php
@@ -11,11 +11,9 @@ $heading = "Note";
 $note = $db->query("SELECT * FROM notes WHERE id = :id", 
   [
     "id" => $_GET['id']
-  ])->fetch();
+  ]
+)->fetchOrAbort();
 
-
-if(!$note || $note["user_id"] !== $currentUserId) {
-  abort(Response::FORBIDDEN);
-}
+authorize($note["user_id"] === $currentUserId, Response::FORBIDDEN);
 
 require "./views/note.view.php";
