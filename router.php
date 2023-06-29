958556c563f3c14328edf6d4b731e64af91f97d0
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-29 15:45:47 +0800

    Update router.php

--- router.php
+++ router.php
@@ -1,16 +1,8 @@
 <?php
 
+$routes = require("routes.php");
+
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
-
-
-// Routes
-$routes = [
-  "/" => "controllers/index.php",
-  "/about" => "controllers/about.php",
-  "/contact" => "controllers/contact.php",
-  "/notes" => "controllers/notes.php",
-  "/note" => "controllers/note.php"
-];
 
 if(array_key_exists($uri, $routes)) {
   require $routes[$uri];
