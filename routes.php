23eb512888a9570ddfa9e7102f2ddf9ae223775e
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-29 15:45:51 +0800

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// Routes
+return [
+  "/" => "controllers/index.php",
+  "/about" => "controllers/about.php",
+  "/contact" => "controllers/contact.php",
+  "/notes" => "controllers/notes.php",
+  "/note" => "controllers/note.php",
+  "/notes/create" => "controllers/note-creation.php"
+];
