cd254b5684c8815157f1c694dab11c4e2d637f15
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-29 17:40:36 +0800

    Update notes.view.php

--- views/notes.view.php
+++ views/notes.view.php
@@ -5,17 +5,17 @@
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <ul>
-      <?php foreach ($notes as $note): ?>
-        <a href="/note?id=<?=$note["id"];?>" class="text-blue-500 hover:underline">
-          <li class="list-none"><?=$note["body"];?></li>
+      <?php foreach ($notes as $note) : ?>
+        <a href="/note?id=<?= $note["id"]; ?>" class="text-blue-500 hover:underline">
+          <li class="list-none"><?= htmlspecialchars($note["body"]); ?></li>
         </a>
       <?php endforeach; ?>
     </ul>
     <p class="mt-6">
-      <a href="#" class="text-blue-500 hover:underline">Create Note</a>
+      <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
     </p>
   </div>
-  
+
 </main>
 
 <?php require("partials/foot.php"); ?>
