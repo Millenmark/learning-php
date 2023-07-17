c471b1be252518b1d430ee4f5dbebb3e5c5ca640
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-17 15:38:01 +0800

    Create note.php

---
+++ controllers/notes/note.php
@@ -0,0 +1,20 @@
+<?php
+
+$config = require("config/config.php");
+$db = new Database($config["database"]);
+
+$currentUserId = 3;
+
+
+$heading = "Note";
+
+$note = $db->query(
+  "SELECT * FROM notes WHERE id = :id",
+  [
+    "id" => $_GET['id']
+  ]
+)->fetchOrAbort();
+
+authorize($note["user_id"] === $currentUserId, Response::FORBIDDEN);
+
+require "./views/notes/note.view.php";
