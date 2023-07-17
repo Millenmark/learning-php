aeb815ec1ad476d83bef04a4597d7f659ec37e1a
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-17 17:00:27 +0800

    Update index.php

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -1,10 +1,10 @@
 <?php
 
-$config = require("config/config.php");
+$config = require("../config/config.php");
 $db = new Database($config["database"]);
-
-$heading = "My Notes";
 
 $notes = $db->query("SELECT * FROM notes WHERE user_id = 3")->fetchAll();
 
-require "./views/notes/index.view.php";
+view("notes/index.view.php", [
+  "heading" => "My Notes"
+]);
