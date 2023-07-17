8e9729a4a310875353f2d0b0097cf3e41029700c
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-17 17:23:01 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,12 @@
 const BASE_PATH = __DIR__ .  "/../";
 
 require BASE_PATH . "./utils/functions.php";
-require base_path("config/Database.php");
-require base_path("Response.php");
+// require base_path("config/Database.php");
+// require base_path("Response.php");
+
+spl_autoload_register(function ($class) {
+  // dd($class);
+  require base_path("config/" . $class . ".php");
+});
+
 require base_path("router.php");
