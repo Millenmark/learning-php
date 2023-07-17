902444e22d1686d5a1d7033a9cd94dbcb6a4016b
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-17 16:53:12 +0800

    update

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+const BASE_PATH = __DIR__ .  "/../";
+
+require BASE_PATH . "./utils/functions.php";
+require base_path("config/Database.php");
+require base_path("Response.php");
+require base_path("router.php");
