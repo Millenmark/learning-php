d96a8bbb8d8576e8361c348a2b3c24f77c5166cf
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-17 17:22:57 +0800

    Update index.php

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -6,5 +6,6 @@ $db = new Database($config["database"]);
 $notes = $db->query("SELECT * FROM notes WHERE user_id = 3")->fetchAll();
 
 view("notes/index.view.php", [
-  "heading" => "My Notes"
+  "heading" => "My Notes",
+  "notes" => $notes
 ]);
