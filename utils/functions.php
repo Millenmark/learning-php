67077ad44638521498969ad86484e4d4964852f6
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-17 16:53:19 +0800

    update

--- utils/functions.php
+++ utils/functions.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
 
-function urlIs($value) {
+function urlIs($value)
+{
   return $_SERVER["REQUEST_URI"] === $value;
 }
 
-function dd($value) {
+function dd($value)
+{
   echo "<pre>";
   var_dump($value);
   echo "</pre>";
@@ -12,14 +14,27 @@ function dd($value) {
   die();
 }
 
-function abort($statusCode = 404) {
+function abort($statusCode = 404)
+{
   http_response_code($statusCode);
   require "views/$statusCode.php";
   die();
 }
 
-function authorize($condition, $status = Response::NOT_FOUND) {
-  if(!$condition) {
+function authorize($condition, $status = Response::NOT_FOUND)
+{
+  if (!$condition) {
     abort($status);
   }
+}
+
+function base_path($path)
+{
+  return BASE_PATH . $path;
+}
+
+function view($path, $attributes = [])
+{
+  extract($attributes);
+  require base_path("views/" . $path);
 }
