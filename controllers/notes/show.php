e286ecad75c11bcfa3f4f1c29fc7c82f312435da
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-18 10:22:42 +0800

    Update show.php

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,20 +7,37 @@ $config = require base_path("config/config.php");
 $db = new Database($config["database"]);
 
 $currentUserId = 3;
-
-
 $heading = "Note";
 
-$note = $db->query(
-  "SELECT * FROM notes WHERE id = :id",
-  [
-    "id" => $_GET['id']
-  ]
-)->fetchOrAbort();
 
-authorize($note["user_id"] === $currentUserId, Response::FORBIDDEN);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $note = $db->query(
+    "SELECT * FROM notes WHERE id = :id",
+    [
+      "id" => $_GET['id']
+    ]
+  )->fetchOrAbort();
 
-view("notes/show.view.php", [
-  "heading" => "Note",
-  "note" => $note
-]);
+  authorize($note["user_id"] === $currentUserId, Response::FORBIDDEN);
+  // form was submitted. Delete the current note.
+  $db->query("delete from notes where id = :id", [
+    "id" => $_GET["id"]
+  ]);
+
+  header('location: /notes');
+  exit();
+} else {
+  $note = $db->query(
+    "SELECT * FROM notes WHERE id = :id",
+    [
+      "id" => $_GET['id']
+    ]
+  )->fetchOrAbort();
+
+  authorize($note["user_id"] === $currentUserId, Response::FORBIDDEN);
+
+  view("notes/show.view.php", [
+    "heading" => "Note",
+    "note" => $note
+  ]);
+}
